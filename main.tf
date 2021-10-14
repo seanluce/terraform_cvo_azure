5dcbe960b8e99e3950b7f4a86598f253959c95c6
Sean Luce <lucesean@gmail.com>
2021-10-14 12:26:48 -0400

    added custom role

--- main.tf
+++ main.tf
@@ -20,6 +20,131 @@ provider "netapp-cloudmanager" {
   refresh_token = var.cm_refresh_token
 }
 
+data "azurerm_subscription" "azure_sub" {
+}
+
+# Create the custom role definition based on 
+resource "azurerm_role_definition" "cm_custom_role" {
+  name        = "NetApp Custom Role"
+  scope       = data.azurerm_subscription.azure_sub.id
+  description = "This is a custom role created via Terraform for NetApp Cloud Manager"
+
+  permissions {
+    actions     = ["Microsoft.Compute/disks/delete",
+                        "Microsoft.Compute/disks/read",
+                        "Microsoft.Compute/disks/write",
+                        "Microsoft.Compute/locations/operations/read",
+                        "Microsoft.Compute/locations/vmSizes/read",
+                        "Microsoft.Resources/subscriptions/locations/read",
+                        "Microsoft.Compute/operations/read",
+                        "Microsoft.Compute/virtualMachines/instanceView/read",
+                        "Microsoft.Compute/virtualMachines/powerOff/action",
+                        "Microsoft.Compute/virtualMachines/read",
+                        "Microsoft.Compute/virtualMachines/restart/action",
+                        "Microsoft.Compute/virtualMachines/deallocate/action",
+                        "Microsoft.Compute/virtualMachines/start/action",
+                        "Microsoft.Compute/virtualMachines/vmSizes/read",
+                        "Microsoft.Compute/virtualMachines/write",
+                        "Microsoft.Compute/images/write",
+                        "Microsoft.Compute/images/read",
+                        "Microsoft.Network/locations/operationResults/read",
+                        "Microsoft.Network/locations/operations/read",
+                        "Microsoft.Network/networkInterfaces/read",
+                        "Microsoft.Network/networkInterfaces/write",
+                        "Microsoft.Network/networkInterfaces/join/action",
+                        "Microsoft.Network/networkSecurityGroups/read",
+                        "Microsoft.Network/networkSecurityGroups/write",
+                        "Microsoft.Network/networkSecurityGroups/join/action",
+                        "Microsoft.Network/virtualNetworks/read",
+                        "Microsoft.Network/virtualNetworks/checkIpAddressAvailability/read",
+                        "Microsoft.Network/virtualNetworks/subnets/read",
+                        "Microsoft.Network/virtualNetworks/subnets/write",
+                        "Microsoft.Network/virtualNetworks/subnets/virtualMachines/read",
+                        "Microsoft.Network/virtualNetworks/virtualMachines/read",
+                        "Microsoft.Network/virtualNetworks/subnets/join/action",
+                        "Microsoft.Resources/deployments/operations/read",
+                        "Microsoft.Resources/deployments/read",
+                        "Microsoft.Resources/deployments/write",
+                        "Microsoft.Resources/resources/read",
+                        "Microsoft.Resources/subscriptions/operationresults/read",
+                        "Microsoft.Resources/subscriptions/resourceGroups/delete",
+                        "Microsoft.Resources/subscriptions/resourceGroups/read",
+                        "Microsoft.Resources/subscriptions/resourcegroups/resources/read",
+                        "Microsoft.Resources/subscriptions/resourceGroups/write",
+                        "Microsoft.Storage/checknameavailability/read",
+                        "Microsoft.Storage/operations/read",
+                        "Microsoft.Storage/storageAccounts/listkeys/action",
+                        "Microsoft.Storage/storageAccounts/read",
+                        "Microsoft.Storage/storageAccounts/delete",
+                        "Microsoft.Storage/storageAccounts/regeneratekey/action",
+                        "Microsoft.Storage/storageAccounts/write",
+                        "Microsoft.Storage/storageAccounts/blobServices/containers/read",
+                        "Microsoft.Storage/usages/read",
+                        "Microsoft.Compute/snapshots/write",
+                        "Microsoft.Compute/snapshots/read",
+                        "Microsoft.Compute/availabilitySets/write",
+                        "Microsoft.Compute/availabilitySets/read",
+                        "Microsoft.Compute/disks/beginGetAccess/action",
+                        "Microsoft.MarketplaceOrdering/offertypes/publishers/offers/plans/agreements/read",
+                        "Microsoft.MarketplaceOrdering/offertypes/publishers/offers/plans/agreements/write",
+                        "Microsoft.Network/loadBalancers/read",
+                        "Microsoft.Network/loadBalancers/write",
+                        "Microsoft.Network/loadBalancers/delete",
+                        "Microsoft.Network/loadBalancers/backendAddressPools/read",
+                        "Microsoft.Network/loadBalancers/backendAddressPools/join/action",
+                        "Microsoft.Network/loadBalancers/frontendIPConfigurations/read",
+                        "Microsoft.Network/loadBalancers/loadBalancingRules/read",
+                        "Microsoft.Network/loadBalancers/probes/read",
+                        "Microsoft.Network/loadBalancers/probes/join/action",
+                        "Microsoft.Authorization/locks/*",
+                        "Microsoft.Network/routeTables/join/action",
+                        "Microsoft.NetApp/netAppAccounts/read",
+                        "Microsoft.NetApp/netAppAccounts/capacityPools/read",
+                        "Microsoft.NetApp/netAppAccounts/capacityPools/volumes/write",
+                        "Microsoft.NetApp/netAppAccounts/capacityPools/volumes/read",
+                        "Microsoft.NetApp/netAppAccounts/capacityPools/volumes/delete",
+                        "Microsoft.Network/privateEndpoints/write",
+                        "Microsoft.Storage/storageAccounts/PrivateEndpointConnectionsApproval/action",
+                        "Microsoft.Storage/storageAccounts/privateEndpointConnections/read",
+                        "Microsoft.Storage/storageAccounts/managementPolicies/read",
+                        "Microsoft.Storage/storageAccounts/managementPolicies/write",
+                        "Microsoft.Network/privateEndpoints/read",
+                        "Microsoft.Network/privateDnsZones/write",
+                        "Microsoft.Network/privateDnsZones/virtualNetworkLinks/write",
+                        "Microsoft.Network/virtualNetworks/join/action",
+                        "Microsoft.Network/privateDnsZones/A/write",
+                        "Microsoft.Network/privateDnsZones/read",
+                        "Microsoft.Network/privateDnsZones/virtualNetworkLinks/read",
+                        "Microsoft.Resources/deployments/operationStatuses/read",
+                        "Microsoft.Insights/Metrics/Read",
+                        "Microsoft.Compute/virtualMachines/extensions/write",
+                        "Microsoft.Compute/virtualMachines/extensions/delete",
+                        "Microsoft.Compute/virtualMachines/extensions/read",
+                        "Microsoft.Compute/virtualMachines/delete",
+                        "Microsoft.Network/networkInterfaces/delete",
+                        "Microsoft.Network/networkSecurityGroups/delete",
+                        "Microsoft.Resources/deployments/delete",
+                        "Microsoft.Compute/diskEncryptionSets/read",
+                        "Microsoft.Compute/snapshots/delete",
+                        "Microsoft.Network/privateEndpoints/delete",
+                        "Microsoft.Compute/availabilitySets/delete",
+                        "Microsoft.Network/loadBalancers/delete",
+                        "Microsoft.KeyVault/vaults/read",
+                        "Microsoft.KeyVault/vaults/accessPolicies/write",
+                        "Microsoft.Compute/diskEncryptionSets/write",
+                        "Microsoft.KeyVault/vaults/deploy/action",
+                        "Microsoft.Compute/diskEncryptionSets/delete",
+                        "Microsoft.Resources/tags/read",
+                        "Microsoft.Resources/tags/write",
+                        "Microsoft.Resources/tags/delete"]
+    not_actions = []
+  }
+
+  assignable_scopes = [
+    data.azurerm_subscription.azure_sub.id
+  ]
+}
+
 # Create the NetApp Cloud Manager service connector resource group
 resource "azurerm_resource_group" "cm_connector_rg" {
   name     = var.cm_connector_rg
@@ -94,9 +219,6 @@ resource "netapp-cloudmanager_connector_azure" "cm_connector" {
   
 }
 
-data "azurerm_subscription" "azure_sub" {
-}
-
 data "azurerm_virtual_machine" "cm_connector_vm" {
   depends_on          = [netapp-cloudmanager_connector_azure.cm_connector]
   name                = var.cm_connector_name
@@ -105,7 +227,7 @@ data "azurerm_virtual_machine" "cm_connector_vm" {
 
 resource "azurerm_role_assignment" "cm_role_assignment" {
   scope              = data.azurerm_subscription.azure_sub.id
-  role_definition_name = "Contributor"
+  role_definition_id = azurerm_role_definition.cm_custom_role.id
   principal_id       = data.azurerm_virtual_machine.cm_connector_vm.identity.0.principal_id
 }
 
